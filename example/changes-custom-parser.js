39112dc62c75e766d0b253786da78cafc7690ff4
Stephen Bartell <snbartell@gmail.com>
2013-06-10 15:32:21 -0700

    rewrite

--- example/changes-custom-parser.js
+++ example/changes-custom-parser.js
@@ -2,8 +2,9 @@ var Stream = require('../index')
 var es = require('event-stream')
 
 var stream = Stream
-  ( 'http://localhost:5984/test/_changes'
-  , { json:true
+  ( 'http://localhost:5984/test/_changes?include_docs=true'
+  , { json: true
+    , parser: ['results', true, 'doc'] //JSONStream style parsing array/str.
     }
   )
 if (!stream)
--- index.js
+++ index.js
@@ -3,46 +3,77 @@ var lib = require('./lib')
 var url = require('url')
 var parsers = lib.parsers
 var querystring = require('querystring')
+var request = require('request')
+var through = require('through')
+var errs = require('errs')
+var inherits = require('util').inherits
+var jsonstream = require('JSONStream')
 
-module.exports = function (uri, opts) {
+module.exports = function CS (uri, opts) {
+  if (!opts)
+    opts = {json: false, parser: null}
+
   var u = url.parse(uri)
-  this.host = u.hostname
-  this.port = u.port
-  this.auth = u.auth
-  this.protocol = u.protocol
-  this.path = u.pathname
+  var qs = querystring.parse(u.query)
+  var feed = qs.feed || 'normal'
 
   // figure out what kind of stream this is
   var m
-  this.type = (m = this.path.match(/_changes|_all_docs/))
+  var type = (m = u.pathname.match(/_changes|_all_docs/))
   ? m[0]
-  : (m = this.path.match(/(_design)\/.*\/(_view).*/))
+  : (m = u.pathname.match(/(_design)\/.*\/(_view).*/))
     ? 'view'
-    : (m = this.path.match(/(_design)\/.*\/(_filter).*/))
+    : (m = u.pathname.match(/(_design)\/.*\/(_filter).*/))
       ? 'filter'
       : null
 
-  var qs = this.qs = querystring.parse(u.query)
-  this.feed = qs.feed || 'normal'
-  this.since = qs.since || 0
-
-  // // figure out how to parse this thing.
-  if (!opts)
-    opts = {json: true, parser: undefined}
-
-  this.parser = null
+  // figure out how to parse it.
+  var parser
   if (opts.json)
-    this.parser = ('_changes' == this.type)
-    ? ('continuous' == this.feed)
-      ? parsers.continuous
-      : parsers.longpoll
-    : ('_all_docs' == this.type || 'view' == this.type || 'filter' == this.type)
-      ? parsers.all_docs
-      : parsers.other
+    if (opts.parser)
+      parser = jsonstream.parse(opts.parser)
+    else
+      parser = ('_changes' == type)
+      ? ('continuous' == feed)
+        ? parsers.continuous()
+        : parsers.longpoll()
+      : ('_all_docs' == type || 'view' == type || 'filter' == type)
+        ? parsers.all_docs()
+        : parsers.other()
 
 
-  var downstream = lib.stream(uri)
-  downstream.parse = this.parser
+  var downstream = through()
+
+  process.nextTick(function () {
+    try {
+      var req = request(uri)
+      .on('error', function (er) {
+        downstream.emit('error', errs.merge(er, 
+          {scope: 'socket'}
+        ))
+      })
+      .on('response', function (res) {
+        if (res.statusCode >= 400)
+          res.on('data', function (data) {
+            downstream.emit('error', errs.create(
+              { message: JSON.parse(data)
+              , code: res.statusCode
+              , scope: 'couch'
+              }))
+          })
+        else
+          (parser) 
+          ? res.pipe(parser).pipe(downstream)
+          : res.pipe(downstream)
+      })
+
+    } catch (er) {
+      downstream.emit('error', errs.merge(er,
+        {scope: 'request'}
+      ))
+    }
+  })
+    
+
   return downstream
 }
-
